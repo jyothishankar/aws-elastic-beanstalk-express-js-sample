25f04d6f39d01bcef93f12ae8a0dfbcf308d2d4b
jyothishankar <jyothishankar.m@mail.com>
2024-02-11 15:14:09 +0000

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('I am here to become Devops Engineer, and will earn in crores! I am working smart and hard for it'));
+app.get('/', (req, res) => res.send('I am here to become Devops Engineer, and will earn in crores! I am working smart and hard for it, and ensure that i will get into Devops very soon'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
