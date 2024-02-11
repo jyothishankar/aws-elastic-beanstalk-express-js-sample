b2b41d572ba2829f81ccf68402fa3168429c6b2a
jyothishankar <jyothishankar.m@mail.com>
2024-02-11 14:15:09 +0000

    change message 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('I am here to become Devops Engineer, and will earn in crores!'));
+app.get('/', (req, res) => res.send('I am here to become Devops Engineer, and will earn in crores! I am working smart and hard for it'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
